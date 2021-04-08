f6c871b9fa3aef46a1f738967ee0fa6e5d7c3df2
Lucas Abrantes <lucaasabrantes002@gmail.com>
2021-04-07 21:47:20 -0300

    Initial files, config and constants

---
+++ src/main/java/com/lucas/abrantes/herosapi/constans/HeroesConstant.java
@@ -0,0 +1,8 @@
+package com.lucas.abrantes.herosapi.constans;
+
+public class HeroesConstant {
+    public static final String HERDES_ENDPONT_LOCAL="/heroes";
+    public static final String ENDPOINT_DYNAMO="http://localhost:8000";
+    public static final String REGION_DYNAMO="us-west-2";
+
+}
